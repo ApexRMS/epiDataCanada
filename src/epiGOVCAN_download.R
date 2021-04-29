c2807634170e1c541506612cdc899c2c9c6fbf18
vlucet <VLucet@users.noreply.github.com>
2021-04-29 15:09:26 +0000

    script now loads and filter data

--- src/epiGOVCAN_download.R
+++ src/epiGOVCAN_download.R
@@ -1,1 +1,31 @@
 ## DownloadGovCan Transformer Script
+
+library(rsyncrosim)
+
+# Environment -------------------------------------------------------------
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(readr)
+
+# Load environment
+
+E <- ssimEnvironment()
+LIB <- ssimLibrary()
+SCE <- scenario()
+
+# TRANSFER_DIR <- e$TransferDirectory
+
+TRANSFORMER_NAME <- "Canadian COVID-19 Data: Download from Canadian Government"
+VARS <- c("Cases", "Tested", 
+          "Cases - Active", "Cases - Recovered", 
+          "Deaths - Daily", "Deaths - Cumulative")
+
+# Source helpers ----------------------------------------------------------
+
+source(file.path(E$PackageDirectory, "epiGOVCAN_helpers.R"))
+
+# 1. Load data
+
+filtered_data <- load_inputs_govcan(SCE)
